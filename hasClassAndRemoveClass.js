21306d2f1ea3727873e497a9cf1e6c734441d567
username <username@email.com>
2016-09-05 23:08:25 +0800

    20160905

---
+++ hasClassAndRemoveClass.js
@@ -0,0 +1,9 @@
+/**
+ * Created by 田 on 2016.09.05.0005.
+ */
+$(document).ready(function(){
+        $("#home").addClass("selected");
+    $("button").click(function(){
+     $("#home").removeClass("selected");
+  });
+});
