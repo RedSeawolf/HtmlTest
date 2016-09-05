30e48b86ace84e1536d5c4534f633f57b80268f7
username <username@email.com>
2016-09-06 00:21:54 +0800

    20160905

---
+++ toggleClass.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2016.09.06.0006.
+ */
+$(document).ready(function(){
+    $("#home").on("click",function(){
+        $("#home").toggleClass("selected")//用鼠标在生成的页面上点击就可以
+    });
+});
