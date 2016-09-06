381212005bdb569158845d23eccbec35e8109002
username <username@email.com>
2016-09-06 21:36:08 +0800

    20150906

--- 获取或设置元素的HTML1.js
+++ 获取或设置元素的HTML1.js
@@ -1,7 +1,7 @@
 /**
  * Created by Administrator on 2016/9/6 0006.
  */
-$("document").ready(function(){
+$(document).ready(function(){
    //$("p").text("");
       $("body").html("<h1>这是添加的一个段落！</h1> <em>这是添加的另一个段落</em>");//This is my Demo
 });
