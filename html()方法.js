d715051dad125914aff2a2a7e9b6a2aad4e2a931
username <username@email.com>
2016-09-09 10:58:15 +0800

    20160909

---
+++ html()方法.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2016/9/8 0008.
+ */
+$(document).ready(function(){
+    $("button").on("click",function(){
+        $("p").html("<strong>没有了下次再来</strong>");
+    });
+});
